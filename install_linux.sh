ec1a13e4e59010c493bfa0d7b3a378b7d418af78
madprops <easywest@protonmail.com>
2019-12-03 17:54:22 -0600

    Install script mod

--- install_linux.sh
+++ install_linux.sh
@@ -3,7 +3,12 @@ echo "Binary placed in /bin"
 
 if [[ -d ~/.config/lq ]]
 then
-    :
+    read -p "Do you want to replace the config file with a default one? (y, n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        cp lq.conf ~/.config/lq/lq.conf
+    fi
 else
     mkdir ~/.config/lq
     cp lq.conf ~/.config/lq/lq.conf
